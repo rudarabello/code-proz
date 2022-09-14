ace20644878139a7b36007957517e63f814b016a
ruda <ruda.rabello.silva@gmail.com>
2022-09-14 11:00:13 -0300

    feat: change socio

--- proz-project-1/lib/socio.dart
+++ proz-project-1/lib/socio.dart
@@ -1,26 +1,36 @@
-import 'package:proz_project_1/endereco.dart';
-
 abstract class Socio {
   String nome;
-  final String _documento;
-  Endereco endereco;
-  Socio(
-    this.documento,{
-      required this.nome,
-      required this.endereco,
-    });
-  Socio cadastrar ({
-    required String nome,
-    required String numeroDocumento,
-    required Endereco endereco,
+  String codigoUnico;
+  String cep;
+  String rua;
+  int numero;
+  String bairro;
+  String cidade;
+  String estado;
+  Socio({
+    required this.nome,
+    required this.codigoUnico,
+    required this.cep,
+    required this.rua,
+    required this.numero,
+    required this.bairro,
+    required this.cidade,
+    required this.estado,
   });
+
   String get documento {
-    if (_documento.length == 11) {
-      return "${_documento.substring(0, 3).${_documento.substring(3,6).${_documento.substring(6,9).${_documento.substring(10,12)}";
-    }else if(_documento.length == 14){
-      return "${_documento.substring(0, 3).${_documento.substring(3,6).${_documento.substring(6,9).${_documento.substring(10,12)}";
-    }else{
+    if (codigoUnico.length == 11) {
+      return "${codigoUnico.substring(0, 3)}.${codigoUnico.substring(3, 6)}.${codigoUnico.substring(6, 9)}.${codigoUnico.substring(10, 12)}";
+    } else if (codigoUnico.length == 14) {
+      return "${codigoUnico.substring(0, 3)}.${codigoUnico.substring(3, 6)}.${codigoUnico.substring(6, 9)}.${codigoUnico.substring(10, 12)}";
+    } else {
       return "Documento inválido";
     }
-    
+  }
+
+  String get cepFormatado => '${cep.substring(0, 5)}-${cep.substring(5)}';
+
+  String get endereco =>
+      '$rua, $numero, $bairro, $cidade/$estado, $cepFormatado';
+
 }
